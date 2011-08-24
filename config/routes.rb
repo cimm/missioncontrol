fccb0f8a6a86f059963f0636cd3aca081b632198
Simon Schoeters <hamfilter@gmail.com>
2011-08-24 08:19:49 +0200

    Remove unused new_unit_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Invoicr::Application.routes.draw do
-  resources :units,   :except => [:destroy] # TODO No new acction I think
+  resources :units,   :except => [:new, :destroy]
   resources :clients, :only => [:index, :new, :create]
 end
