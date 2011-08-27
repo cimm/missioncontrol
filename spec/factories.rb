bdde5fc8b43f5c962595be3f42abc3f7b980ebb4
Simon Schoeters <hamfilter@gmail.com>
2011-08-27 09:50:13 +0200

    Move to Sham for FactoryGirl values

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,14 @@
+require 'factory_girl/syntax/sham'
+
+Sham.executed_at {|n| Date.today + n.days}
+Sham.nickname    {|n| "Acme #{n}"}
+
 FactoryGirl.define do
   factory :unit do
-    executed_at Date.today
+    executed_at
   end
 
   factory :client do
-    nickname "Acme" # TODO Use shams for the nickname
+    nickname
   end
 end
