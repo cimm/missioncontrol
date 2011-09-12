f6edf60c9375d6abdc66a18344eb1a570e9efe4f
Simon Schoeters <hamfilter@gmail.com>
2011-09-13 00:52:07 +0200

    Fix Rails initializer to support Ruby 1.8.7

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-MissionControl::Application.config.session_store :cookie_store, key: '_mission_control_session'
+MissionControl::Application.config.session_store :cookie_store, :key => '_mission_control_session'
